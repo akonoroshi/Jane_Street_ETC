1bdd0cda27b2886b503c2d13d84ff68ef7c944fb
浅野裕哉 <akounoroushi@Yuyas-MacBook-Air.local>
2019-06-08 13:16:19 -0400

    implemented buy() and sell()

--- sample-bot-Yuya.py
+++ sample-bot-Yuya.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# ~~~~==============   HOW TO RUN   ==============~~~~
+# ~~~~~==============   HOW TO RUN   ==============~~~~~
 # 1) Configure things in CONFIGURATION section
 # 2) Change permissions: chmod +x bot.py
 # 3) Run in loop: while true; do ./bot.py; sleep 1; done
@@ -13,9 +13,7 @@ import json
 import time
 #import numpy as np
 
-from Dyna_Q import *
-
-# ~~~~============== CONFIGURATION  ==============~~~~
+# ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
 team_name="TOURISTS"
 # This variable dictates whether or not the bot is connecting to the prod
@@ -32,7 +30,7 @@ prod_exchange_hostname="production"
 port=25000 + (test_exchange_index if test_mode else 0)
 exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
 
-# ~~~~============== NETWORKING CODE ==============~~~~
+# ~~~~~============== NETWORKING CODE ==============~~~~~
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((exchange_hostname, port))
@@ -46,13 +44,9 @@ def read_from_exchange(exchange):
     return json.loads(exchange.readline())
 
 
-# ~~~~============== MAIN LOOP ==============~~~~
+# ~~~~~============== MAIN LOOP ==============~~~~~
+
 def main():
-    actions = ['buy', 'sell', 'nothing']
-    gamma = 0.8
-
-    dynaQ = Dyna_Q(actions, gamma)
-
     exchange = connect()
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
@@ -61,21 +55,11 @@ def main():
     # Since many write messages generate marketdata, this will cause an
     # exponential explosion in pending messages. Please, don't do that!
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
-
+    actions = ["add", "cancel"]
     symbols = ["BOND", "GS", "MS", "USD", "VALBZ", "VALE", "WFC", "XLF"]
 
-
-    while True:
-        # TODO send price information to function
-        # TODO function(price)
-        # TODO get_decision
-        # TODO find the best price for buying the
-        # TODO buy
-        # TODO 一定時間がたってrewardを送る
-        ## learning
-
-        from_exchange = read_from_exchange(exchange)
-
+    #while True:
+        # TODO replace
     action = "add"
     id = 0
     symbol = symbols[id]
@@ -85,23 +69,6 @@ def main():
     write_to_exchange(exchange, {"type": action, "order_id": id, "symbol": symbol, "dir": "SELL", "price": 1002, "size": 50})
 
 
-def get_price(symbol: str, exchange_info: dict) -> list:
-    pass
 
-def buy(symbol: str, price: int, id: int, exchange):
-    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": symbol, "dir": "BUY", "price": price, "size": 50})
-
-
-def sell(symbol: str, price: int, id: int, exchange):
-    write_to_exchange(exchange, {"type": "add", "order_id": id, "symbol": symbol, "dir": "SELL", "price": price, "size": 50})
-
-
-def fair_price_calculator(symbol, exchange_info):
-    pass
-
-
-def reward_calculator():
-    pass
-
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
