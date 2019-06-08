8dcf3ce68c7906d65ad156399eff477a9e043fad
浅野裕哉 <akounoroushi@Yuyas-MacBook-Air.local>
2019-06-07 23:02:53 -0400

    implemented Dyna-Q

--- Dyna_Q.py
+++ Dyna_Q.py
@@ -1,19 +1,22 @@
 import numpy as np
 import pandas as pd
 import math
+from operator import add
 
 class Dyna_Q:
     def __init__(self, actions, gamma, alpha=0.2):
         self.actions = actions # List
         self.alpha = alpha # Learning rate
-        self.gamma = gamma # Discount rate
-        self.q_table = pd.DataFrame(colmuns=self.actions) # states x actions
-        self.num_act = {act : 0.00001 for act in self.actions} # Avoid division by 0
-        self.tc = np.full()
+        self.gamma = gamma # Discount rate (close to 0: more immediate reward, 1: more long-term reward)
+        self.q_table = pd.DataFrame(columns=self.actions) # states x actions
+        self.tc = {} # count of (state, action, state_), used to calculate probability below
+        self.t = {} # transition function: probability of seeing state_ given state and action
+        self.r = {} # reward function: expected reward of a certain action at a given state
 
+    # If the state is new, add it
     def add_state(self, state):
         if state not in self.q_table.index:
-            # append new state to q table
+            # Append the new state to q table
             self.q_table = self.q_table.append(
                 pd.Series(
                     np.random.rand(len(self.actions)) * 0.01,
@@ -22,37 +25,50 @@ class Dyna_Q:
                 )
             )
 
-    def choose_action(self, observation, t):
+            # Add the new state to T and R
+            self.tc[state] = {act : {state_ : 0.00001 for state_ in self.q_table.index} for act in self.actions}
+            self.t[state] = {act : {state_ : 1 / len(self.states) for state_ in self.q_table.index} for act in self.actions}
+            self.r[state] = {act : 0 for act in self.actions}
+
+    # Do exploitation and exploration using UCB1 algorithm
+    def choose_action(self, observation, c=1):
         # Precondition: observation (state) is a dicrete value
         self.add_state(observation)
 
-        # UCB1 (see my notes on kibela)
+        # UCB1
         max_value = 0
         max_action = None
+        num_state = sum(sum(self.tc[observation][act].values() for act in self.actions))
+        if num_state < 1:
+            num_state = 1
         for act in self.actions:
-            if self.q_table.ix[observation, act] + math.sqrt(2 * math.log(t) / self.num_act[act]) > max_value:
+            if self.q_table.ix[observation, act] + c * math.sqrt(2 * math.log(num_state) / self.tc[observation][act]) > max_value:
                 max_action = act
                 max_value = self.q_table.ix[observation, act]
-        self.num_act[max_action] = self.num_act[max_action] + 1
 
         return max_action
 
     # Happens after choosing action and observing a new state
     def learn(self, s, a, r, s_, num_iter=100):
+        # Update model
         self.add_state(s_)
-        update_Q(self, s, a, r, s_)
+        self.update_Q(s, a, r, s_)
+        self.tc[s][a][s_] += 1
+        self.t[s][a][s_] = self.tc[s][a][s_] / np.sum(self.tc[s][a])
+        self.r[s][a] = (1 - self.alpha) * self.r[s][a] + self.alpha * r
 
         # Simulation
         for i in range(num_iter):
-            state = self.generate_state()
+            # Choose states and actions randomly
+            state = np.random.choice(self.q_table.index)
             action = np.random.choice(self.actions)
+
+            # Get a new state and reward according to current functions and update Q
+            state_ = np.random.choice(self.q_table.index, p=list(self.t[state][action].key))
+            reward = self.r[state][action]
+            self.update_Q(state, action, reward, state_)
         
 
     def update_Q(self, s, a, r, s_):
         q_target = r + self.gamma * self.q_table.ix[s_, :].max()
-        self.q_table.ix[s, a] += self.lr * (q_target - self.q_table.ix[s, a])
-
-    def generate_state(self):
-        state = 0 # TODO
-        self.add_state(state)
-        return state
+        self.q_table.ix[s, a] += self.alpha * (q_target - self.q_table.ix[s, a])
